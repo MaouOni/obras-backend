ff5f0c2c211f389144ade63ca8fc1368e1875a76
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-27 22:06:52 -0600

    Dynamic values

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,15 @@
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
-    port: process.env.PG_PORT,
-});
+
+const sequelize = new Sequelize(
+    process.env.PG_DATABASE,
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    {
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        dialect: 'postgres',
+    }
+);
 
 const db = {};
 
