ea30d3413ae892800b03814a71f8649aa980ee01
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-28 00:08:36 -0600

    Making sure to update config

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        username: 'root',
+        password: 'tlTaGftauvitqVwh9z5GB1mBPb1JjtlK',
+        database: 'obras_dxgb',
+        host: 'dpg-cpv3bflumphs73c4td60-a',
+        dialect: 'postgres',
+    },
+};
