f7b6c3062da770733a7e2038fdaefed5e3b72683
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-28 11:56:37 -0600

    Render Port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const catalogoRoutes = require('./routes/catalogoRoutes');
 const estimacionRoutes = require('./routes/estimacionRoutes');
 
 const app = express();
-const port = process.env.PORT || '10000';
+const port = process.env.PORT || 10000;  // Use Render's default port
 
 app.use(cors());
 app.use(bodyParser.json());
